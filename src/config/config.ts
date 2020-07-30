5340c796532a1bdf86b45466adbceb5759f157e7
Anubhav Mishra <anubmish@adobe.com>
2020-07-30 10:20:48 +0530

    Code Depoyed on AWS Elastic Beanstalk

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  dev: {
+    username: process.env.POSTGRESS_USERNAME,
+    password: process.env.POSTGRESS_PASSWORD,
+    database: process.env.POSTGRESS_DATABASE,
+    host: process.env.POSTGRESS_HOST,
+    dialect: process.env.POSTGRESS_DIALECT,
+    aws_region: process.env.POSTGRESS_AWS_REGION,
+    aws_profile: process.env.POSTGRESS_AWS_PROFILE,
+    aws_media_bucket: process.env.POSTGRESS_AWS_MEDIA_BUCKET,
+  },
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+  jwt: {
+    secret: process.env.POSTGRESS_JWT_SECRET,
+  },
+};
